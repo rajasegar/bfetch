a08fac66a80f77e79b510f67cd390cb407efa822
Rajasegar <rajasegar.c@gmail.com>
2021-02-08 13:15:14 +0530

    chore: add filesize package
    1. add user info

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const scheme = options.theme || 'Dracula';
 const colors = require(`@blessed/themes/themes/${scheme}`);
 const theme = require('./styles')(colors.colors);
 const si = require('systeminformation');
+const filesize = require('filesize');
 
 module.exports = function () {
   const program = blessed.program();
@@ -33,6 +34,7 @@ module.exports = function () {
     height: '100%',
     border: theme.box.border,
     style: theme.box.style,
+    tags: true,
   });
 
   screen.key(['q'], () => {
@@ -43,9 +45,8 @@ module.exports = function () {
   screen.append(rightBox);
 
   let info = '';
-  info += `Uptime: \n`;
+
   info += `Packages: \n`;
-  info += `Shell: \n`;
   info += `DE: \n`;
   info += `WM: \n`;
   info += `WM theme: \n`;
@@ -56,6 +57,9 @@ module.exports = function () {
     osInfo: 'codename, release, build, arch, kernel',
     mem: 'used, total',
     graphics: '*',
+    shell: '*',
+    time: 'uptime',
+    users: '*',
   };
 
   si.get(valueObject).then((data) => {
@@ -67,16 +71,34 @@ module.exports = function () {
       osInfo: { codename, release, build, arch, kernel },
       mem: { used, total },
       graphics: { controllers, displays },
+      shell,
+      time: { uptime },
+      users,
     } = data;
 
     const gpus = controllers.map((c) => c.model).join(', ');
-    info += `OS: ${codename} ${release} ${build} ${arch}\n`;
-    info += `Host: ${model}\n`;
-    info += `Kernel: ${kernel}\n`;
-    info += `Resolution: ${displays[0].resolutionX}x${displays[0].resolutionY}\n`;
-    info += `CPU: ${manufacturer} ${brand} (${cores}) @ ${speed}GHz\n`;
-    info += `GPU: ${gpus}\n`;
-    info += `Memory: ${used} / ${total}\n`;
+    const label = (txt) => `{bold}{yellow-fg}${txt}:{/}`;
+    const userInfo = `{bold}{green-fg}${users[0].user}{/}@xxxx\n`;
+    info += userInfo;
+    info +=
+      userInfo
+        .split('')
+        .map((x) => '-')
+        .join('') + '\n';
+    info += `${label('OS')} ${codename} ${release} ${build} ${arch}\n`;
+    info += `${label('Host')} ${model}\n`;
+
+    info += `${label('Kernel')} ${kernel}\n`;
+    info += `${label('Uptime')} ${uptime}\n`;
+    info += `${label('Shell')} ${shell}\n`;
+    info += `${label('Resolution')} ${displays[0].resolutionX}x${
+      displays[0].resolutionY
+    }\n`;
+    info += `${label(
+      'CPU'
+    )} ${manufacturer} ${brand} (${cores}) @ ${speed}GHz\n`;
+    info += `${label('GPU')} ${gpus}\n`;
+    info += `${label('Memory')} ${filesize(used)} / ${filesize(total)}\n`;
 
     rightBox.content = info;
     screen.render();
